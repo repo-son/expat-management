29668cafe326d89ab469262fb9626021c5aebbd5
repo-son <lahiru980529@gmail.com>
2024-03-12 16:14:11 +0300

    Complete User settings on dashboard

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,6 +5,7 @@ import { ChevronsLeft, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { useRef, ElementRef, useState, useEffect } from "react";
 import { useMediaQuery } from "usehooks-ts";
+import { UserItem } from "./user-item";
 
 export const Navigation = () => {
   const pathname = usePathname();
@@ -110,7 +111,7 @@ export const Navigation = () => {
           <ChevronsLeft className="h-6 w-6"/>
         </div>
         <div>
-          <p>Action Items</p>
+          <UserItem/>
         </div>
         <div className="mt-4">
           <p>Documents</p>
